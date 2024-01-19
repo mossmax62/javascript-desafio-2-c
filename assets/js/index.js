521ceee63b81379bdf2e58c311cf642cbab588b3
Maximiliano Mussuto <mossmax62@gmail.com>
2024-01-19 12:55:13 -0300

    initial commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,37 @@
 let suma = 0;
-let input1 = document.querySelector('.box1 input');
-let input2 = document.querySelector('.box2 input');
-let input3 = document.querySelector('.box3 input');
-let h3 = document.querySelector('h3');
+let input1 = document.querySelector('.box1 select');
+let input2 = document.querySelector('.box2 select');
+let input3 = document.querySelector('.box3 select');
+let button = document.querySelector('button');
+let h2 = document.querySelector('h2');
 suma = Number(input1.value) + Number(input2.value) + Number(input3.value);
 console.log(suma);
 
 input1.addEventListener('change', () => {
-    checkSuma();
+    //checkPassword();
 });
 input2.addEventListener('change', () => {
-    checkSuma();
+    //checkPassword();
 });
 input3.addEventListener('change', () => {
-    checkSuma();
+    //checkPassword();
 });
 
-function checkSuma() {
+button.addEventListener('click',()=>{
+    checkPassword();
+})
+
+function checkPassword() {
     console.log('Checking...');
-    suma = Number(input1.value) + Number(input2.value) + Number(input3.value);
-    if (suma <= 10) {
-        h3.style.color = 'green';
-        h3.innerHTML = "Llevas : " + suma + "Stickers";
+    seleccion = input1.value + input2.value + input3.value;
+    if (seleccion === '911') {
+        h2.style.color = 'green';
+        h2.innerHTML = "Password 1 correcto";
+    } else if (seleccion === '714'){
+        h2.style.color = 'green';
+        h2.innerHTML = "Password 2 correcto";
     } else {
-        h3.style.color = 'red';
-        h3.innerHTML = "Llevas demasiados Stickers";
+        h2.style.color = 'red';
+        h2.innerHTML = "Password incorrecto";
     }
 }
